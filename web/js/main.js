edc22111039694b448c93c032aabbfc0b78513b5
shiinedev <shiinecadde17@gmail.com>
2024-11-17 08:18:12 +0300

    add event in humbergur

--- web/js/main.js
+++ web/js/main.js
@@ -2,3 +2,7 @@ const navLinks =document.querySelector(".nav-links")
 const humbergur = document.querySelector(".humbergur")
 const icons = document.querySelectorAll("i")
 
+
+humbergur.addEventListener('click', function(event){
+    
+ })
