5251347e6942ffa4ef0804545bce3d0e82015511
Hayle01 <mabdirahim832@gmail.com>
2024-12-11 14:14:02 +0300

    added Change PIN factionality

--- extension/dash.js
+++ extension/dash.js
@@ -40,6 +40,31 @@ const modalMessage = document.querySelector("#modalMessage");
 const modalInput = document.querySelector("#modalInput");
 const modalForm = document.querySelector("#modalForm");
 const errorMessage = document.querySelector("#errorMessage");
+const inputs = document.querySelectorAll(".pin-input input");
+
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    // Allow only numeric input
+    const value = e.target.value.replace(/[^0-9]/g, "");
+    e.target.value = value;
+
+    // Automatically move to the next input if valid number is entered
+    if (value.length === 1 && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    // Handle Backspace behavior
+    if (e.key === "Backspace" && input.value === "") {
+      if (index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
+});
+
 
 // change PIN
 const changePIN = document.querySelector("#changePIN");
@@ -47,18 +72,25 @@ const changePIN = document.querySelector("#changePIN");
 changePIN.addEventListener("click", () => {
   modal.style.display = "flex";
   modalTitle.textContent = "Change PIN";
-  modalInput.type = "password";
-  modalInput.placeholder = "Fadlan gali PIN-kaaga"
+  modalMessage.textContent = "Fadlan gali PIN-kaaga";
 })
 
 modalForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  if(modalInput.value)
-  if (modalInput.value.length !== 4) {
-     showError("PIN must be exactly 4 digits long");
-     return;
-  }else{
-    window.location.href = "/dashboard.html"
+   let isEmpty = false;
+    inputs.forEach(input => {
+        if (input.value.trim() === "") {
+            isEmpty = true;
+        }
+    });
+    if (isEmpty) {
+        showError("Fill All the inputs");
+        return
+    }else{
+     modalMessage.textContent = "Fadlan Gali PIN-kaaga Cusub";
+     inputs.forEach(input => {
+      input.value = ''
+     })
   }
 });
 
@@ -72,5 +104,6 @@ function showError(message) {
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    errorMessage.style.display = "none";
   }
 };
